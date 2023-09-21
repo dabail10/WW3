51abe09c5b9ebc65b999a1c112acd47dcaaf368a
alperaltuntas <alperaltuntas@gmail.com>
2023-09-21 14:58:49 -0600

    rename wav_shel.nml as wav_in

--- model/src/wav_shel_inp.F90
+++ model/src/wav_shel_inp.F90
@@ -167,6 +167,7 @@ contains
     character(len=80)   :: linein
     character(len=30)   :: ofile ! w3_cou only
     character(len=8)    :: words(7)=''
+    character(len=256)  :: filename
     logical             :: flflg, flhom, tflagi, prtfrm, flgnml, flh(-7:10)
     integer             :: thrlev = 1
     integer             :: time0(2), timen(2), ttime(2)
@@ -258,16 +259,17 @@ contains
     ! Read nml file if available
     !--------------------
 
-    inquire(file=trim(fnmpre)//"ww3_shel.nml", exist=flgnml)
+    filename = trim(fnmpre)//"wav_in"
+    inquire(file=trim(filename), exist=flgnml)
 
     if (flgnml) then
-      open(newunit=ndsi, file=trim(fnmpre)//"ww3_shel.nml", status='old', iostat=ierr)
+      open(newunit=ndsi, file=trim(filename), status='old', iostat=ierr)
 
       !--------------------
       ! Read namelist
       !--------------------
 
-      call w3nmlshel (mpi_comm, ndsi, trim(fnmpre)//'ww3_shel.nml', nml_domain, nml_input, &
+      call w3nmlshel (mpi_comm, ndsi, trim(filename), nml_domain, nml_input, &
            nml_output_type, nml_output_date, nml_homog_count, nml_homog_input, ierr)
 
       !--------------------
